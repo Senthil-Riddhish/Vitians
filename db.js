9bb0e63fafc80bef150c9fae06fe0d223a765081
Riddhishwar senthil <82893678+Senthil-Riddhish@users.noreply.github.com>
2021-11-21 13:07:59 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { connect } = require('http2');
 const mongoose=require('mongoose');
-const url="mongodb+srv://riddhishwar:Mayurie@cluster0.son3w.mongodb.net/Vitians?retryWrites=true&w=majority";
+const url="mongodb+srv://riddhish:@cluster0.son3w.mongodb.net/Vitians?retryWrites=true&w=majority";
 const db=mongoose.connect(url,{
     useUnifiedTopology:true,
     useNewUrlParser:true
